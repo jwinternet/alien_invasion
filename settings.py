253710431bf430d21a4444283055f870ee4e0d3e
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-03-03 22:02:18 -0500

    Adding settings module.

---
+++ settings.py
@@ -0,0 +1,25 @@
+"""
+Alien Invasion Settings Module
+"""
+###############################################################################
+
+__author__ = "Jared Winter"
+__copyright__ = "Copyright 2020, jwinternet"
+__credits__ = ""
+__license__ = "GNU General Public License v3.0"
+__version__ = "0.0.1"
+__updated__ = "3/3/2020"
+__email__ = "jaredwinter2015@outlook.com"
+__status__ = "DEV"
+
+###############################################################################
+
+class Settings:
+	"""A class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""Initialize the game's settings."""
+		# Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
