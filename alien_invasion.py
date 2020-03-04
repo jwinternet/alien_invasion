d51be0e702ee96b1751e661146cff412c9b04b70
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-03-04 03:29:31 -0500

    Further updates.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,7 +8,7 @@ __copyright__ = "Copyright 2020, jwinternet"
 __credits__ = ""
 __license__ = "GNU General Public License v3.0"
 __version__ = "0.0.1"
-__updated__ = "3/3/2020"
+__updated__ = "3/4/2020"
 __email__ = "jaredwinter2015@outlook.com"
 __status__ = "DEV"
 
