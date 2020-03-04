55ed6e35b5f45f1f92aee0070d732bfbdae55827
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-03-03 22:01:28 -0500

    Added colors!

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
 """
-Alien Invasion Program
+Alien Invasion Primary Module
 """
 ###############################################################################
 
@@ -16,6 +16,7 @@ __status__ = "DEV"
 
 import sys
 import pygame
+import settings
 
 ###############################################################################
 
@@ -25,12 +26,15 @@ class AlienInvasion:
 	def __init__(self):
 		"""Initialize the game, and create game resources."""
 		pygame.init()
+		self.settings = settings.Settings()
 
-		self.screen = pygame.display.set_mode((1200, 800))
+		self.screen = pygame.display.set_mode(
+				(
+					self.settings.screen_width,
+					self.settings.screen_height
+					)
+				)
 		pygame.display.set_caption("Alien Invasion")
-
-		# Set the background color.
-		self.bg_color = (230, 230, 230)
 
 	def run_game(self):
 		"""Start the main look for the game."""
@@ -41,7 +45,7 @@ class AlienInvasion:
 					sys.exit()
 
 			# Redraw the screen during each pass through the loop.
-			self.screen.fill(self.bg_color)
+			self.screen.fill(self.settings.bg_color)
 
 			# Make the most recently drawn screen visible.
 			pygame.display.flip()
