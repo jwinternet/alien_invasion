c448acee7ec445aec735aae41ebd0f707acfd7ad
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-03-03 20:14:39 -0500

    testing

---
+++ alien_invasion.py
@@ -0,0 +1,48 @@
+"""
+Alien Invasion Program
+"""
+###############################################################################
+
+__author__ = "Jared Winter"
+__copyright__ = "Copyright 2020, jwinternet"
+__credits__ = ""
+__license__ = "GNU General Public License v3.0"
+__version__ = "0.0.1"
+__updated__ = "3/3/2020"
+__email__ = "jaredwinter2015@outlook.com"
+__status__ = "DEV"
+
+###############################################################################
+
+import sys
+import pygame
+
+
+###############################################################################
+
+class AlienInvasion:
+	"""Overall class to manage game assets and behavior."""
+
+	def __init__(self):
+		"""Initialize the game, and create game resources."""
+		pygame.init()
+
+		self.screen = pygame.display.set_mode((1200, 800))
+		pygame.display.set_caption("Alien Invasion")
+
+	def run_game(self):
+		"""Start the main look for the game."""
+		while True:
+			# Watch for keyboard and mouse events.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			# Make the most recently drawn screen visible.
+			pygame.display.flip()
+
+
+if __name__ == '__main__':
+	# Make a game instance, and run the game.
+	ai = AlienInvasion()
+	ai.run_game()
