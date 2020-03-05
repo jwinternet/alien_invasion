8c69ae62a740591c7bea3a3a0d51726e90e04537
jwinternet <47834941+Killface0@users.noreply.github.com>
2020-03-04 21:25:21 -0500

    The alien ships now move back and forth while edging closer.

--- settings.py
+++ settings.py
@@ -26,6 +26,9 @@ class Settings:
 
 		# Ship settings
 		self.ship_speed = 1.5
+		self.fleet_drop_speed = 10
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
 
 		# Bullet settings
 		self.bullet_speed = 1.0
@@ -33,3 +36,6 @@ class Settings:
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 15
+
+		# Alien settings
+		self.alien_speed = 0.5
